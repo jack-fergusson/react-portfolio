193c091b96902b38ab025382ab8b62eebc15d7a0
Jack Fergusson <jack.fergusson03@gmail.com>
2025-10-24 11:32:05 -0400

    updated navbar transition.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import Navbar from "./Navbar.jsx";
 import Projects from "./Projects.jsx";
 import Clubs from "./Clubs.jsx";
 import More from "./More.jsx";
+import { useRef } from "react";
 
 const App = () => {
+  const watcher = useRef(null);
+
   return (
     <main className="main container">
+      <div ref={watcher}></div>
       <Hero />
-      <Navbar />
+      <Navbar watcher={watcher} className="sticking" />
       <Projects />
       <Clubs />
+      <More />
+      <More />
+      <More />
       <More />
       <More />
       <More />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,23 @@
 import { headers } from "./data";
+import { useEffect, useRef } from "react";
 
-const Navbar = () => {
+const Navbar = ({ watcher }) => {
+  const navbarRef = useRef(null);
+
+  useEffect(() => {
+    watcher.current.setAttribute("data-scroll-watcher", "");
+    navbarRef.current.before(watcher.current);
+    navObserever.observe(watcher.current);
+  }, []);
+
+  const navObserever = new IntersectionObserver((entries) => {
+    // console.log("Hello!");
+    navbarRef.current.classList.toggle("sticking", !entries[0].isIntersecting);
+    watcher.current.classList.toggle("move-up", !entries[0].isIntersecting);
+  });
+
   return (
-    <section className="navbar">
+    <section className="navbar" ref={navbarRef}>
       <div className="divider"></div>
       <div className="headers container">
         {headers.map((header, index) => {
